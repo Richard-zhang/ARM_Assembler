8592d56274a6adecb16ff96bb06684b71fe2829e
Ryan Chung <rc2815@matrix34.doc.ic.ac.uk>
2016-05-20 17:38:43 +0100

    Created a "createMask" function.
    This function creates a mask of specified length between two bits.

--- src/emulate.c
+++ src/emulate.c
@@ -1,5 +1,30 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <assert.h>
+
+uint32_t createMask(uint32_t bot, uint32_t top);
+int branch(uint32_t instr);
 
 int main(int argc, char **argv) {
   return EXIT_SUCCESS;
 }
+	
+int branch(uint32_t instr) {
+    /* branch is a helper function that contains an 24 bit 2s complement
+       offset that will be sign extended and added to "PC" */
+}
+
+uint32_t createMask(uint32_t bot, uint32_t top) {
+    /* createMask is a helper function that produces a mask (with correct
+       offset) */
+    assert(bot <= top); 
+    uint32_t mask = 0;
+    int i;
+
+    for(i = bot; i <= top; i++) {
+        mask |= 1 << i;
+    }
+    return mask;
+}    
