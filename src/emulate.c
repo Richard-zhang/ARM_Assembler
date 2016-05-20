26da09cc4c2eca82453768a786625213261be804
Ryan Chung <rc2815@matrix27.doc.ic.ac.uk>
2016-05-20 23:46:56 +0100

    Added a checkCond function.
    Added two helper functions to check Z bits and for equality of N and V bits.

--- src/emulate.c
+++ src/emulate.c
@@ -5,6 +5,9 @@
 
 uint32_t createMask(uint32_t bot, uint32_t top);
 int branch(uint32_t instr);
+int checkCond(uint32_t cond, uint32_t CPSRpntr);
+int checkZ(uint32_t CPSRpntr);
+int equalityNV(uint32_t CPSRpntr);
 
 int main(int argc, char **argv) {
     assert(argc == 2); 
@@ -31,6 +34,37 @@ int main(int argc, char **argv) {
     }
     
     return 0;    
+}
+
+int checkCond(uint32_t cond, uint32_t CPSRpntr) {
+    /* This function is used to check the condition of the instruction.
+    It calls helper functions checkZ and equalityNV to return a value
+    of 1 if the condition is met, or 0 if the condition is not met. */
+    switch(cond) {
+        case 0  : return checkZ(CPSRpntr); 
+        case 1  : return !checkZ(CPSRpntr);
+        case 10 : return equalityNV(CPSRpntr);
+        case 11 : return !equalityNV(CPSRpntr);
+        case 12 : return (!checkZ(CPSRpntr) && equalityNV(CPSRpntr));
+        case 13 : return (checkZ(CPSRpntr) || !equalityNV(CPSRpntr));
+        default : assert(cond == 14); return 1;
+    } 
+}
+
+int checkZ(uint32_t CPSRpntr) {
+    /* This function checks the 30th bit of the CPSR register (the Z
+       bit) to see if it is set. Returns 1 if Z is set. Returns 0 if
+       it is clear. */
+    return getBits(30, 30, CPSRpntr);
+}
+
+int equalityNV(uint32_t CPSRpntr) {
+    /* This function checks the 31st bit of the CPSR register (the N
+       bit) and compares it with the 28th bit (the V bit). Returns 1 of
+       N = C. Returns 0 if N != C */
+    int N = getBits(31, 31, CPSRpntr);
+    int V = getBits(28, 28, CPSRpntr);
+    return N == V;
 }
 	
 int branch(uint32_t instr) {
