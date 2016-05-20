7f38546e2bfbfa6dbc163d08146160e85ee54462
Christopher Conlan <christopher.conlan15@imperial.ac.uk>
2016-05-20 17:18:29 +0100

    Binary reader implemented.
    We implemented the binary reader in the main function.

--- src/emulate.c
+++ src/emulate.c
@@ -1,5 +1,31 @@
 #include <stdlib.h>
+#include <stdint.h> 
+#include <stdio.h> 
+#include <assert.h>
 
 int main(int argc, char **argv) {
-  return EXIT_SUCCESS;
+    assert(argc == 2); 
+
+    // The maximum capacity of the ARM machine memory is 64KB
+    const int MAX_SIZE = 65536;
+    
+    // The maximum number of entries in the main memory 
+    const int MAX_ENTRIES = MAX_SIZE / sizeof(int);     
+
+    // Reserving space for main memory 
+    uint32_t *mainMem = calloc(MAX_ENTRIES, sizeof(int)); 
+     
+    // Declaring the binary file  
+    FILE *binFile; 
+     
+    // Defining the binary file with fopen 
+    binFile = fopen(argv[1], "rb");
+    
+    if (binFile) {    
+    fread(mainMem, sizeof(int), MAX_ENTRIES, binFile); 
+    } else {
+    perror("File not found!");
+    }
+
+    return 0;    
 }
